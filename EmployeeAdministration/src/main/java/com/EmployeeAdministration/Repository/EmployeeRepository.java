6dbac99778a8fcf8f26d5ef6e68356cfae3818d3
saiteja-143 <86522627+saiteja-143@users.noreply.github.com>
2021-10-27 17:59:20 +0530

    Delete EmployeeAdministration directory

--- EmployeeAdministration/src/main/java/com/EmployeeAdministration/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.EmployeeAdministration.Repository;
-
-import com.EmployeeAdministration.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
--- EmployeeAdministration/src/main/java/com/EmployeeAdministration/exception/EmployeeException.java
+++
@@ -1,8 +0,0 @@
-package com.EmployeeAdministration.exception;
-
-public class EmployeeException extends RuntimeException{
-
-    public EmployeeException(String exception) {
-        super(exception);
-    }
-}
